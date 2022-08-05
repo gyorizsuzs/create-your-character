d3b73c5225b35c88287bd58f946ab6abb7f834f0
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-08-05 02:28:10 +0200

    base script

---
+++ frontend/script.js
@@ -0,0 +1,214 @@
+/* COMPONENTS */
+
+const inputName = document.querySelector(".input-name");
+const displayName = document.querySelector(".display-name");
+inputName.addEventListener("input", (event) => {
+  let inputNameData = event.target.value;
+  displayName.innerHTML = inputNameData;
+});
+
+const femaleDisplayComponent = function () {
+  return /*html*/ `
+    <i class="fa-solid fa-venus"></i>
+    `;
+};
+
+const maleDisplayComponent = function () {
+  return /*html*/ `
+    <i class="fa-solid fa-mars"></i>
+    `;
+};
+
+const otherDisplayComponent = function () {
+  return /*html*/ `
+    <i class="fa-solid fa-blender"></i>
+    `;
+};
+
+const genderOptions = [
+  femaleDisplayComponent,
+  maleDisplayComponent,
+  otherDisplayComponent,
+];
+
+const selectGender = document.querySelector("#select-gender");
+const displayGender = document.querySelector(".display-gender");
+selectGender.addEventListener("click", (event) => {
+  let selectGenderData = event.target.value;
+  for (let i = 0; i < genderOptions.length; i++) {
+    displayGender.insertAdjacentHTML("beforeend", genderOptions[i]);
+  }
+});
+
+const blackComponent = function () {
+  return /*html*/ `
+  <img src="./images/blnumenoreans.webp>"
+  `;
+};
+const dwarvesComponent = function () {
+  return /*html*/ `
+  <img src="./images/Dwarves.webp>"
+  `;
+};
+const elvesComponent = function () {
+  return /*html*/ `
+  <img src="./images/elves.webp>"
+  `;
+};
+const hobbitsComponent = function () {
+  return /*html*/ `
+  <img src="./images/Hobbits.webp>"
+  `;
+};
+const nazgulComponent = function () {
+  return /*html*/ `
+  <img src="./images/nazgul.webp>"
+  `;
+};
+const ologhaiComponent = function () {
+  return /*html*/ `
+  <img src="./images/ologhai.webp>"
+  `;
+};
+const menComponent = function () {
+  return /*html*/ `
+  <img src="./images/Talion.webp>"
+  `;
+};
+const undeadComponent = function () {
+  return /*html*/ `
+  <img src="./images/Undead.webp>"
+  `;
+};
+const urukComponent = function () {
+  return /*html*/ `
+  <img src="./images/Uruk.webp>"
+  `;
+};
+
+const raceOptions = [
+  blackComponent,
+  dwarvesComponent,
+  elvesComponent,
+  hobbitsComponent,
+  nazgulComponent,
+  menComponent,
+  urukComponent,
+  undeadComponent,
+  ologhaiComponent,
+];
+
+const selectRace = document.querySelector("#select-race");
+const displayRace = document.querySelector(".display-race");
+selectRace.addEventListener("click", (event) => {
+  let selectRaceData = event.target.value;
+  displayRace.innerHTML = selectRaceData;
+
+  displayRace.insertAdjacentHTML("beforeend", raceOptions[i]);
+});
+
+const beastComponent = function () {
+  return /*html*/ `
+  <i class="fa-solid fa-paw-claws"></i>
+  `;
+};
+const executionerComponent = function () {
+  return /*html*/ `
+  <i class="fa-solid fa-skull"></i>
+  `;
+};
+const rangerComponent = function () {
+  return /*html*/ `
+  <i class="fa-solid fa-user-cowboy"></i>
+  `;
+};
+const slaverComponent = function () {
+  return /*html*/ `
+  <i class="fa-solid fa-handcuffs"></i>
+  `;
+};
+const sawboneComponent = function () {
+  return /*html*/ `
+  <i class="fa-solid fa-bone-break"></i>
+  `;
+};
+
+const classOptions = [
+  beastComponent,
+  executionerComponent,
+  raceOptions,
+  slaverComponent,
+  sawboneComponent,
+];
+
+const selectClass = document.querySelector("#select-class");
+const displayClass = document.querySelector(".display-class");
+selectClass.addEventListener("click", (event) => {
+  let selectClassData = event.target.value;
+  displayClass.innerHTML = selectClassData;
+  displayClass.insertAdjacentHTML("beforeend", classOptions[i]);
+});
+
+/* DROPDOWN MENUS */
+
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+  const select = dropdown.querySelector(".select");
+  const menu = dropdown.querySelector(".menu");
+  const options = dropdown.querySelectorAll("option");
+  /*     const selected = dropdown.querySelector('.selected'); */
+
+  select.addEventListener("click", () => {
+    select.classList.toggle("select-clicked");
+    menu.classList.toggle("menu-open");
+  });
+
+  options.forEach((option) => {
+    option.addEventListener("click", () => {
+      select.innerText = option.innerText;
+      select.classList.remove("select-clicked");
+      menu.classList.remove("menu-open");
+      options.forEach((option) => {
+        option.classList.remove("active");
+      });
+    });
+  });
+});
+
+/* LOAD EVENT */
+/* 
+function loadEvent() {
+    console.log('loadEvent execution');
+    const root =document.querySelector('root');
+
+    root.insertAdjacentHTML('beforeend', inputComponent());
+    root.insertAdjacentHTML('beforeend', displayComponent());
+
+    const inputName = document.querySelector('.input-name');
+    const displayName = document.querySelector('.display-name');
+    inputName.addEventListener('input', (event) => {
+        let inputNameData = event.target.value;
+        displayName.innerHTML = inputNameData;
+    });
+
+    const selectGender = document.querySelector('#select-gender');
+    const displayGender = document.querySelector('.display-gender');
+    selectGender.addEventListener('click', (event) => {
+        displayGender.insertAdjacentHTML('beforeend', femaleDisplayComponent(), maleDisplayComponent(), otherDisplayComponent());
+    });
+
+    const selectRace = document.querySelector('#select-race');
+    const displayRace = document.querySelector('.display-race');
+    selectRace.addEventListener('click', (event) => {
+        let selectRaceData = event.target.value;
+        displayRace.innerHTML = selectRaceData;
+    });
+
+    const selectClass = document.querySelector('#select-class');
+    const displayClass = document.querySelector('.display-class');
+    selectClass.addEventListener('click', (event) => {
+        let selectClass = event.target.value;
+        displayClass.innerHTML = selectClassData;
+    });
+}; */
